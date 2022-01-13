bef073c7c10114313d9c65564d609e092caae4a9
juha <lainema@gmail.com>
2022-01-13 09:21:20 +0200

    more logging

--- server.go
+++ server.go
@@ -266,7 +266,7 @@ func (a *App) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if err == nil {
 		context.Set(r, "RequestID", fmt.Sprintf("%x-%x-%x-%x-%x", b[0:4], b[4:6], b[6:8], b[8:10], b[10:]))
 	}
-
+	logger.Log(kv{"fn": "ServeHTTP", "id": context.Get(r, "RequestID")})
 	a.router.ServeHTTP(w, r)
 }
 
